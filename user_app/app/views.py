709ef6851788c659699dba5839ef1ce9ec3eec99
JeanPhilippeGuillard <jeanphilippe.guillard@gmail.com>
2021-02-28 11:11:56 +0100

    Ajout d'une fonction de monitoring de base : - via le package opencensus - envoi d'un warning

--- user_app/app/views.py
+++ user_app/app/views.py
@@ -1,8 +1,23 @@
-from app import app
+from app import app, credentials
 from flask import render_template, redirect, request, url_for
 
 
 
+#------------------------- Test instrumentation -------------------------------
+import logging
+from datetime import datetime
+from opencensus.ext.azure.log_exporter import AzureLogHandler
+
+INSIGHT_CONNECTION_STRING = "InstrumentationKey=" + credentials.INSIGHT_KEY
+
+logger = logging.getLogger(__name__)
+
+logger.addHandler(AzureLogHandler(
+                    connection_string=INSIGHT_CONNECTION_STRING))
+
+utc_time = datetime.utcnow()
+logger.warning("Message d'alerte de views.py à {}".format(utc_time))
+print("message d'alerte envoyé")
 
 #---------------------------- Manage routes -----------------------------------
 
