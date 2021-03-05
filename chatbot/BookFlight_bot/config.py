012da09cc03055a279c042cc9bc8937117562bb2
JeanPhilippeGuillard <jeanphilippe.guillard@gmail.com>
2021-03-05 14:31:53 +0100

    Réorganisation des répertoires

---
+++ chatbot/BookFlight_bot/config.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+import os
+
+""" Bot Configuration """
+
+
+class DefaultConfig:
+    """ Bot Configuration """
+
+    PORT = 3978
+    APP_ID = os.environ.get("MicrosoftAppId", "")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
+    LUIS_APP_ID = os.environ.get("LuisAppId", "4d36e06a-7acf-48d3-a945-6d124d219be2")
+    LUIS_API_KEY = os.environ.get("LuisAPIKey", "2246a01e45d447ceb6f9799e72a75504")
+    # LUIS endpoint host name, ie "westus.api.cognitive.microsoft.com"
+    LUIS_API_HOST_NAME = os.environ.get("LuisAPIHostName", "westeurope.api.cognitive.microsoft.com")
